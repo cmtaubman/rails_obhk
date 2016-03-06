da6ea71c6eb8b9a930ede657f8703adb552b2b20
cmtaubman <carmen@cgkcorporation.com>
2016-03-06 16:40:20 +0800

    fourth commit trying to get indiv bars page to work

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,12 +1,12 @@
 class BarsController < ApplicationController
-#   def index
-#     @bars = Bar.all
-#   end
+  def index
+    @bars = Bar.all
+  end
 
 
-# # GET /bars/1
-#   # GET /bars/1.json
-#   def show
-#   end
+# GET /bars/1
+  # GET /bars/1.json
+  def show
+  end
 
 end
