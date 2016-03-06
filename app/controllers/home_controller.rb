25e27c4588cf36a0747e5da4ca39a2002b45f024
cmtaubman <carmen@cgkcorporation.com>
2016-03-06 12:42:32 +0800

    database additions and homepage erb added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,15 @@
 class HomeController < ApplicationController
 
   def homepage
-
+    @bars = []
+    all_bars = Bar.all
+    all_bars.each_with_index do |bar, i|
+      if i % 3 == 0
+        @bars << [bar]
+      else
+        @bars[i/3] << bar
+      end
+    end
   end
 
 end
