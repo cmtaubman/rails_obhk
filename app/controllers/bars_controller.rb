6314dac54f002ea7e05ad72f8f642da52e048257
cmtaubman <carmen@cgkcorporation.com>
2016-03-06 17:55:05 +0800

    bar controller edit

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,4 +1,6 @@
 class BarsController < ApplicationController
+  before_action :set_bar
+
   def index
     @bars = Bar.all
   end
@@ -7,6 +9,13 @@ class BarsController < ApplicationController
 # GET /bars/1
   # GET /bars/1.json
   def show
+  end
+
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_bar
+    @bar = Bar.find(params[:id])
   end
 
 end
